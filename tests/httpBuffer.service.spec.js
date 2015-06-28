09e26956df66ed9a1ee5c839a5edd0849005f9e6
Adam Buczynski <adambuczynski@gmail.com>
2015-06-28 19:11:59 +1200

    Test and editor config

---
+++ tests/httpBuffer.service.spec.js
@@ -0,0 +1,14 @@
+
+/**
+ * Specifications
+ */
+describe('Http buffer', function() {
+
+  //Load module
+  beforeEach(module('Utility.HttpBuffer.Service'));
+
+  //TODO: tests
+  it('should write a unit test for the http buffer', function() {
+    expect(true).toBe(true);
+  });
+});
