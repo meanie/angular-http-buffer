4cb22b0cb9e93ab3cb49acb565465963b613c1da
Adam Buczynski <adambuczynski@gmail.com>
2016-07-18 19:49:11 +1200

    New release

--- release/meanie-angular-http-buffer.js
+++ release/meanie-angular-http-buffer.js
@@ -1,103 +1,106 @@
 /**
- * meanie-angular-http-buffer - v1.1.1 - 10-1-2016
+ * meanie-angular-http-buffer - v1.1.1 - 18-6-2016
  * https://github.com/meanie/angular-http-buffer
  *
  * Copyright (c) 2016 Adam Buczynski <me@adambuczynski.com>
  * License: MIT
  */
-(function(window, angular, undefined) {'use strict';
+'use strict';
 
-/**
- * Module definition and dependencies
- */
-angular.module('HttpBuffer.Service', [])
-
-/**
- * Service definition
- */
-.factory('$httpBuffer', ['$q', '$injector', function($q, $injector) {
-
-  //Requests buffer
-  var buffer = [];
-
-  //HTTP service, initialized later due to circular dependency
-  var $http;
+(function (window, angular, undefined) {
+  'use strict';
 
   /**
-   * Helper to retry a http request
+   * Module definition and dependencies
    */
-  function retryHttpRequest(config, deferred) {
 
-    //Get the http service now
-    $http = $http || $injector.get('$http');
-
-    //Retry the request
-    $http(config).then(function(response) {
-      deferred.resolve(response);
-    }, function(reason) {
-      deferred.reject(reason);
-    });
-  }
+  angular.module('HttpBuffer.Service', [])
 
   /**
-   * Service class
+   * Service definition
    */
-  return {
+  .factory('$httpBuffer', ['$q', '$injector', function ($q, $injector) {
+
+    //Requests buffer
+    var buffer = [];
+
+    //HTTP service, initialized later due to circular dependency
+    var $http;
 
     /**
-     * Store a new request in the buffer
+     * Helper to retry a http request
      */
-    store: function(config) {
-      var deferred = $q.defer();
-      buffer.push({
-        config: config,
-        deferred: deferred
+    function retryHttpRequest(config, deferred) {
+
+      //Get the http service now
+      $http = $http || $injector.get('$http');
+
+      //Retry the request
+      $http(config).then(function (response) {
+        deferred.resolve(response);
+      }, function (reason) {
+        deferred.reject(reason);
       });
-      return deferred.promise;
-    },
+    }
 
     /**
-     * Clear the buffer (without rejecting requests)
+     * Service class
      */
-    clear: function() {
-      buffer = [];
-    },
+    return {
 
-    /**
-     * Reject all the buffered requests
-     */
-    rejectAll: function(reason) {
+      /**
+       * Store a new request in the buffer
+       */
+      store: function store(config) {
+        var deferred = $q.defer();
+        buffer.push({
+          config: config,
+          deferred: deferred
+        });
+        return deferred.promise;
+      },
 
-      //Loop all buffered requests and reject them
-      for (var i = 0; i < buffer.length; i++) {
-        buffer[i].deferred.reject(reason);
-      }
+      /**
+       * Clear the buffer (without rejecting requests)
+       */
+      clear: function clear() {
+        buffer = [];
+      },
 
-      //Clear the buffer
-      this.clear();
-    },
+      /**
+       * Reject all the buffered requests
+       */
+      rejectAll: function rejectAll(reason) {
 
-    /**
-     * Retry all buffered requests
-     */
-    retryAll: function(configUpdater) {
-
-      //Loop all buffered requests
-      for (var i = 0; i < buffer.length; i++) {
-
-        //Config updater provided? Use it
-        if (configUpdater && angular.isFunction(configUpdater)) {
-          buffer[i].config = configUpdater(buffer[i].config);
+        //Loop all buffered requests and reject them
+        for (var i = 0; i < buffer.length; i++) {
+          buffer[i].deferred.reject(reason);
         }
 
-        //Retry the request
-        retryHttpRequest(buffer[i].config, buffer[i].deferred);
+        //Clear the buffer
+        this.clear();
+      },
+
+      /**
+       * Retry all buffered requests
+       */
+      retryAll: function retryAll(configUpdater) {
+
+        //Loop all buffered requests
+        for (var i = 0; i < buffer.length; i++) {
+
+          //Config updater provided? Use it
+          if (configUpdater && angular.isFunction(configUpdater)) {
+            buffer[i].config = configUpdater(buffer[i].config);
+          }
+
+          //Retry the request
+          retryHttpRequest(buffer[i].config, buffer[i].deferred);
+        }
+
+        //Clear the buffer
+        this.clear();
       }
-
-      //Clear the buffer
-      this.clear();
-    }
-  };
-}]);
-
+    };
+  }]);
 })(window, window.angular);
